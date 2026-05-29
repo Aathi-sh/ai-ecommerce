35d207731da6e7138fa4f188d1ae4373158b1e2d
Punniyamoorthy B <punniyamoorthy.unbots@gmail.com>
2026-05-29 22:24:02 +0530

    Update server.js

--- whatsapp-bot/server.js
+++ whatsapp-bot/server.js
@@ -36,7 +36,8 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   
   if (ALLOWED_ORIGINS.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
